7763396c77af1e9057efcb01789049025a2b56c1
Oksana Moroz <k.moroz.mail@gmail.com>
2023-11-12 16:05:31 +0200

    added styles in root

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./style.module.scss";
+import logo from "../../public/images/logo.png";
 
-interface LogoProps {}
+interface LogoProps {
+  title?: string;
+  className?: string;
+}
 
 export const Logo: React.FC<LogoProps> = ({}) => {
   return (
     <Link href="/" className={styles.logo}>
-      <Image src="/images/logo.png" alt="DUSKTOPIA" width={204} height={204} />
+      <Image src={logo} alt="DUSKTOPIA" width={204} height={204} />
     </Link>
   );
 };
